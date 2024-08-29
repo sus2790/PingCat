8ce6b9986e2b188594c47a30c264a068d1f36e43
`` <amengpan6@gmail.com>
2024-08-29 18:58:28 +0800

    maoyue's code

--- core/core.py
+++ core/core.py
@@ -1,38 +1,58 @@
+import logging
 import os
+
 import arc
 import hikari
+import httpx
 import miru
-import requests
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 arc_client = arc.GatewayClient(bot)
-client = miru.Client.from_arc(arc_client)
+miru_client: miru.Client = miru.Client.from_arc(arc_client)
+httpx_client = httpx.AsyncClient()
+logger: logging.Logger = logging.getLogger("PingCat")
+
 
 @arc_client.include
 @arc.slash_command("ping", "Ping test!")
-async def ping_command(
+async def handle_ping_command(
     ctx: arc.GatewayContext,
     ip: arc.Option[str, arc.StrParams("Which IP do you want to ping?")],
-    host: arc.Option[str, arc.StrParams("Which host do you want to ping from?", choices=["Taipei, Taiwan - AS149791 StarVerse Network Ltd.", "Taichung, Taiwan - AS17809 VEE TIME CORP."])],
-    ipversion: arc.Option[str, arc.StrParams("Your are using ipv4 or ipv6?", choices=["ipv4", "ipv6"])]
-) -> None:
+    host: arc.Option[
+        str,
+        arc.StrParams(
+            "Which host do you want to ping from?",
+            choices=[
+                "Taipei, Taiwan - AS149791 StarVerse Network Ltd.",
+                "Taichung, Taiwan - AS17809 VEE TIME CORP.",
+            ],
+        ),
+    ],
+    ipversion: arc.Option[
+        str,
+        arc.StrParams("Your are using ipv4 or ipv6?", choices=["ipv4", "ipv6"]),
+    ],
+) -> arc.InteractionResponse | None:
     if host == "Taipei, Taiwan - AS149791 StarVerse Network Ltd.":
-        url = os.environ["IP1"]
-        data = {
+        url: str = os.environ["IP1"]
+        data: dict[str, str] = {
             "api_key": os.environ["APIKEY1"],
             "target_ip": ip,
-            "ip_version": ipversion
+            "ip_version": ipversion,
         }
-        response = requests.post(url, data=data)
-        
-        print(f"Status Code: {response.status_code}")
-        print(f"Response Content: {response.text}")
-        
+        response: httpx.Response = await httpx_client.post(url, json=data)
+
+        logger.info(f"Status Code: {response.status_code}")
+        logger.info(f"Response Content: {response.text}")
+
         try:
-            json_response = response.json()
-            formatted_response = f"```\n{json_response.get("output")}\n```"
+            json_response = await response.json()
+            formatted_response = f"```json\n{json_response.get("output")}\n```"
             await ctx.respond(formatted_response)
-        except requests.JSONDecodeError:
-            await ctx.respond(f"Error: Unable to parse JSON response. Raw response:\n```\n{response.text}\n```")
+        except Exception as e:
+            logger.info(e)
+            return await ctx.respond(
+                f"Error: Unable to parse JSON response. Raw response:\n```\n{response.text}\n```",
+            )
     else:
-        await ctx.respond("Host not supported yet")
+        return await ctx.respond("Host not supported yet")
