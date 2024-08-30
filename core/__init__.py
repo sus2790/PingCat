6b17682c06332d1b15652484f79b64de8d6576b5
`` <amengpan6@gmail.com>
2024-08-31 00:03:20 +0800

    erm what the sigma

--- core/__init__.py
+++ core/__init__.py
@@ -1,17 +1,28 @@
-import gc
-
 import dotenv
 
 dotenv.load_dotenv()
-del dotenv
-gc.collect()
 
 from core import core
+
 from core.core import (
     arc_client,
     bot,
+    domain_to_ip,
     handle_ping_command,
+    hosts,
     httpx_client,
+    identify_ip,
+    ping_host,
 )
 
-__all__ = ["arc_client", "bot", "core", "handle_ping_command", "httpx_client"]
+__all__ = [
+    "arc_client",
+    "bot",
+    "core",
+    "domain_to_ip",
+    "handle_ping_command",
+    "hosts",
+    "httpx_client",
+    "identify_ip",
+    "ping_host",
+]
--- core/core.py
+++ core/core.py
@@ -1,51 +1,54 @@
+import ipaddress
 import logging
 import os
-import re
 import socket
 
 import arc
 import hikari
 import httpx
-import miru
+import validators
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 arc_client = arc.GatewayClient(bot)
-miru_client: miru.Client = miru.Client.from_arc(arc_client)
 httpx_client = httpx.AsyncClient()
 logger: logging.Logger = logging.getLogger("PingCat")
 
-ipv4_regex = re.compile(
-    r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
-)
-ipv6_regex = re.compile(
-    r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
-)
-domain_regex = re.compile(
-    r"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.(?!-)[A-Za-z0-9-]{1,63}(?<!-)$"
-)
+hosts = [
+    "Taipei, Taiwan - AS149791 StarVerse Network Ltd.",
+    "Taichung, Taiwan - AS17809 VEE TIME CORP.",
+    "Taoyuan, Taiwan - AS131596 TBC",
+]
 
-def detect_input_type(input_str: str) -> tuple[str, str]:
-    if ipv4_regex.match(input_str):
-        return "ipv4", input_str
-    elif ipv6_regex.match(input_str):
-        return "ipv6", input_str
-    elif domain_regex.match(input_str):
-        return "domain", input_str
-    else:
-        return "invalid", input_str
 
-def resolve_domain_to_ip(domain: str) -> tuple[str, str]:
+def domain_to_ip(
+    domain: str,
+):
     try:
-        ip_address = socket.gethostbyname(domain)
-        if ipv4_regex.match(ip_address):
+        ip_address: str = socket.gethostbyname(domain)
+        ip_obj: ipaddress.IPv4Address | ipaddress.IPv6Address = ipaddress.ip_address(
+            ip_address,
+        )
+        if isinstance(ip_obj, ipaddress.IPv4Address):
             return "ipv4", ip_address
-        elif ipv6_regex.match(ip_address):
-            return "ipv6", ip_address
-        else:
-            return "invalid", ip_address
-    except socket.gaierror as e:
-        logger.error(f"Failed to resolve domain: {e}")
+        return "ipv6", ip_address
+    except socket.gaierror:
+        logger.exception("Failed to resolve domain")
         return "invalid", domain
+    except ValueError:
+        logger.exception("Invalid IP address")
+        return "invalid", domain
+
+
+def identify_ip(input_str: str):
+    if validators.ipv4(input_str):
+        return "ipv4", input_str
+    if validators.ipv6(input_str):
+        return "ipv6", input_str
+    if validators.domain(input_str):
+        ip = domain_to_ip(input_str)
+        return "domain", ip
+    return "invalid", input_str
+
 
 async def ping_host(
     ctx: arc.GatewayContext,
@@ -54,26 +57,43 @@ async def ping_host(
     target: str,
     ip_version: str,
     host: str,
-):
+) -> arc.InteractionResponse:
     data: dict[str, str] = {
-        "api_key": api_key,
         "target_ip": target,
         "ip_version": ip_version,
     }
-    response: httpx.Response = await httpx_client.post(url, json=data)
-
-    logger.info(f"Status Code: {response.status_code}")
-    logger.info(f"Response Content: {response.text}")
+    if not api_key:
+        data.update({"api_key": ""})
 
     try:
+        response: httpx.Response = await httpx_client.post(url, json=data)
+        response.raise_for_status()
+        logger.debug(
+            f"{'=' * 30}\nStatus Code: {response.status_code}\nResponse Content: {response.text}\n{'=' * 30}\n",
+        )
+
+        if not response.text:
+            logger.exception(f"Received an empty response from the server: {e}")
+            return await ctx.respond(
+                "Error: Received an empty response from the server.",
+            )
+
         json_response = response.json()
-        formatted_response = f"Ping result from {host}\n```\n{json_response.get('output')}\n```"
-        await ctx.respond(formatted_response)
+        formatted_response = (
+            f"Ping result from `{host}`\n```\n{json_response.get('output')}\n```"
+        )
+        return await ctx.respond(formatted_response)
+    except httpx.TimeoutException as e:
+        logger.exception(f"Failed to get response from server: {e}")
+        return await ctx.respond(
+            "Timeout while attempting to ping the IP address. Please check if the entered IP address is correct.",
+        )
     except Exception as e:
-        logger.error(f"Failed to parse JSON: {e}")
-        await ctx.respond(
+        logger.exception(f"Failed to parse JSON response: {e}")
+        return await ctx.respond(
             f"Error: Unable to parse JSON response. Raw response:\n```\n{response.text}\n```",
         )
+
 
 @arc_client.include
 @arc.slash_command("ping", "Ping test!")
@@ -84,34 +104,28 @@ async def handle_ping_command(
         str,
         arc.StrParams(
             "Which host do you want to ping from?",
-            choices=[
-                "Taipei, Taiwan - AS149791 StarVerse Network Ltd.",
-                "Taichung, Taiwan - AS17809 VEE TIME CORP.",
-                "Taoyuan, Taiwan - AS131596 TBC"
-            ],
+            choices=hosts,
         ),
     ],
-) -> arc.InteractionResponse | None:
-    ip_version, detected_target = detect_input_type(target)
-
-    if ip_version == "domain":
-        ip_version, detected_target = resolve_domain_to_ip(detected_target)
+) -> arc.InteractionResponse:
+    ip_version, detected_target = identify_ip(target)
 
     if ip_version == "invalid":
-        await ctx.respond("Invalid IP address or domain name.")
-        return
-    
-    if host == "Taipei, Taiwan - AS149791 StarVerse Network Ltd.":
-        url = os.environ["IP1"]
-        api_key = os.environ["APIKEY1"]
-    elif host == "Taichung, Taiwan - AS17809 VEE TIME CORP.":
-        url = "https://api.cowgl.xyz/ping"
-        api_key = ""
-    elif host == "Taoyuan, Taiwan - AS131596 TBC":
-        url = "https://pingapi.milkteamc.org/ping"
-        api_key = ""
-    else:
-        await ctx.respond("Host not supported yet")
-        return
+        return await ctx.respond("Invalid IP address or domain name.")
 
-    await ping_host(ctx, url, api_key, detected_target, ip_version, host)
+    try:
+        host_index = hosts.index(host)
+    except ValueError:
+        return await ctx.respond("Host not supported yet.")
+
+    api_key = None
+
+    if host_index == 0:
+        url: str = os.environ["IP1"]
+        api_key: str = os.environ["APIKEY1"]
+    elif host_index == 1:
+        url = "https://api.cowgl.xyz/ping"
+    elif host_index == 2:
+        url = "https://pingapi.milkteamc.org/ping"
+
+    return await ping_host(ctx, url, api_key, detected_target, ip_version, host)
