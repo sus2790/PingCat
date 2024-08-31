baed61f1ab48a619e584635ac644bc8086bbbf2e
`` <amengpan6@gmail.com>
2024-08-31 21:26:56 +0800

    沒差啊 你就繼續兇我

--- core/core.py
+++ core/core.py
@@ -50,6 +50,7 @@ async def ping_host(
     target: str,
     ip_version: str,
     host: str,
+    private: bool,
 ) -> arc.InteractionResponse:
     data = {
         "target_ip": target,
@@ -61,36 +62,39 @@ async def ping_host(
         try:
             response = await client.post(url, json=data)
             response.raise_for_status()
-        except httpx.TimeoutException as e:
-            logger.exception(f"Failed to get response from server: {e}")
+            json_response = response.json()
+        except httpx.TimeoutException:
+            # `::` always timeout for some reason, shouldn't it return non-success?
             return await ctx.respond(
                 "Error: Timeout while attempting to ping the IP address. Please check if the entered IP address is correct.",
             )
+        except httpx.HTTPStatusError as e:
+            logger.exception(e)
+            json_response = response.json()
+            if private:
+                result = json_response.get("output", "Unknown error occurred.").replace(url, "[HIDDEN]")
+            else:
+                result = json_response.get("output", "Unknown error occurred.")
+            return await ctx.respond(result)
         except Exception as e:
-            logger.exception(f"Failed to parse JSON response: {e} | Payload: {data}")
-            return await ctx.respond(
-                f"Error: Unable to parse JSON response. Raw response:\n```\n{response.text}\n```",
-            )
+            logger.exception(e)
+            json_response = response.json()
+            if private:
+                result = json_response.get("output", "Unknown error occurred.").replace(url, "[HIDDEN]")
+            else:
+                result = json_response.get("output", "Unknown error occurred.")
+            return await ctx.respond(result)
 
-        logger.debug(
-            f"{'=' * 30}\nStatus Code: {response.status_code}\nResponse Content: {response.text}\n{'=' * 30}\n",
+        logger.info(
+            f"{'=' * 30}\nStatus Code: {response.status_code}\nResponse Content: {response.json()}\n{'=' * 30}\n",
         )
-
-        json_response = response.json()
-
-        if not json_response:
-            logger.exception("Received an empty response from the server.")
-            return await ctx.respond(
-                "Error: Received an empty response from the server.",
-            )
 
         if json_response.get("success"):
             formatted_response = (
                 f"Ping result from `{host}`\n```\n{json_response.get('output')}\n```"
-            )
+            ).replace(url, "[HIDDEN]")
             return await ctx.respond(formatted_response)
 
-        logger.warning(f"Ping failed with output: {json_response.get('output')}")
         return await ctx.respond(json_response.get("output", "Unknown error occurred."))
 
 
@@ -110,23 +114,34 @@ async def handle_ping_command(
     ip_version, detected_target = await identify_ip(target)
 
     if ip_version == "invalid":
-        return await ctx.respond(ctx, "Invalid IP address or domain name.")
+        return await ctx.respond("Invalid IP address or domain name.")
 
     if ip_version == "private":
-        return await ctx.respond(ctx, "Private IP addresses are not allowed.")
+        return await ctx.respond("Private IP addresses are not allowed.")
 
     host_info = {
-        0: {"url": os.environ["IP1"], "api_key": os.environ["APIKEY1"]},
-        1: {"url": "https://api.cowgl.xyz/ping", "api_key": ""},
-        2: {"url": "https://pingapi.milkteamc.org/ping", "api_key": ""},
+        "Taipei, Taiwan - AS149791 StarVerse Network Ltd.": {
+            "url": os.environ["IP1"],
+            "api_key": os.environ["APIKEY1"],
+            "private": True,
+        },
+        "Taichung, Taiwan - AS17809 VEE TIME CORP.": {
+            "url": "https://api.cowgl.xyz/ping",
+            "api_key": "",
+            "private": False,
+        },
+        "Taoyuan, Taiwan - AS131596 TBC": {
+            "url": "https://pingapi.milkteamc.org/ping",
+            "api_key": "",
+            "private": False,
+        },
     }
 
-    host_index = hosts.index(host)
+    if host not in host_info:
+        return await ctx.respond("Host not supported yet.")
 
-    if host_index not in host_info:
-        return await ctx.respond(ctx, "Host not supported yet.")
+    url = host_info[host]["url"]
+    api_key = host_info[host]["api_key"]
+    is_private = host_info[host]["private"]
 
-    url = host_info[host_index]["url"]
-    api_key = host_info[host_index]["api_key"]
-
-    return await ping_host(ctx, url, api_key, detected_target, ip_version, host)
+    return await ping_host(ctx, url, api_key, detected_target, ip_version, host, is_private)
